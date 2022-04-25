4bdf70b764d611da738183e0bfedf42c938a8160
ali alahyarlou <alahyarlou@gmail.com>
2022-04-25 14:20:42 +0430

    compleat the project carts and responsive plane

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,14 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     fontFamily: {
-      'sans':['Poppins', 'sans-serif']
+      'sans': ['Poppins', 'sans-serif']
     },
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "2rem",
+      },
+    },
   },
   plugins: [],
 }
